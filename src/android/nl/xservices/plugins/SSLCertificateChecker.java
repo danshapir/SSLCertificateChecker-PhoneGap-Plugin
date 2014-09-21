c9631312c737bc53ae270454ee87487bf04caf8f
danshapir <s.dan@live.com>
2014-09-21 13:46:14 +0300

    Payoneer

--- src/android/nl/xservices/plugins/SSLCertificateChecker.java
+++ src/android/nl/xservices/plugins/SSLCertificateChecker.java
@@ -9,6 +9,7 @@ import javax.net.ssl.HttpsURLConnection;
 import javax.security.cert.CertificateException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
@@ -26,14 +27,30 @@ public class SSLCertificateChecker extends CordovaPlugin {
         public void run() {
           try {
             final String serverURL = args.getString(0);
-            final String allowedFingerprint = args.getString(1);
-            final String allowedFingerprintAlt = args.getString(2);
-            final String serverCertFingerprint = getFingerprint(serverURL);
 
-            if (allowedFingerprint.equalsIgnoreCase(serverCertFingerprint) || allowedFingerprintAlt.equalsIgnoreCase(serverCertFingerprint)) {
+			String joinedFingerPrints = args.getString(1);
+            final String [] allowedFingerprint = joinedFingerPrints.split(",");
+
+            final ArrayList<String> serverCertFingerprints = getFingerprints(serverURL);
+
+			boolean valid = false;
+			for(int i=0; i<serverCertFingerprints.size(); i++) {
+				for(int j=0; j<allowedFingerprint.length; j++) {
+					if(serverCertFingerprints.get(i).equalsIgnoreCase(allowedFingerprint[j])) {
+						valid = true;
+						break;
+					}
+				}
+
+				if(valid) {
+					break;
+				}
+			}
+
+            if (valid) {
               callbackContext.success("CONNECTION_SECURE");
             } else {
-              callbackContext.error("CONNECTION_NOT_SECURE");
+              callbackContext.success("CONNECTION_NOT_SECURE");
             }
           } catch (Exception e) {
             callbackContext.error("CONNECTION_FAILED. Details: " + e.getMessage());
@@ -47,13 +64,71 @@ public class SSLCertificateChecker extends CordovaPlugin {
     }
   }
 
-  private static String getFingerprint(String httpsURL) throws IOException, NoSuchAlgorithmException, CertificateException, CertificateEncodingException {
-    final HttpsURLConnection con = (HttpsURLConnection) new URL(httpsURL).openConnection();
+  private static ArrayList<String> getFingerprints(String httpsURL) throws IOException, NoSuchAlgorithmException, CertificateException, CertificateEncodingException {
+
+	ArrayList<String> result = new ArrayList<String>();
+	boolean urlValid = false;
+
+	// open connection
+	URL targetUrl = new URL(httpsURL);
+	String host = targetUrl.getHost();
+	android.util.Log.w("Certificate", "Connecting to host: " + host);
+	final HttpsURLConnection con = (HttpsURLConnection) targetUrl.openConnection();
     con.connect();
-    final Certificate cert = con.getServerCertificates()[0];
-    final MessageDigest md = MessageDigest.getInstance("SHA1");
-    md.update(cert.getEncoded());
-    return dumpHex(md.digest());
+
+	// get certificates
+    Certificate[] certs = con.getServerCertificates();
+
+	// check certificates
+    android.util.Log.w("Certificate", "Certificate count: " + certs.length);
+    for(int i=0; i<certs.length; i++)
+    {
+        android.util.Log.w("Certificate", "Certificate " + i + " type: " + certs[i].getClass().getName());
+
+		// extract thumbprint
+        MessageDigest d = MessageDigest.getInstance("SHA1");
+        d.update(certs[i].getEncoded());
+        String fingerPrint = dumpHex(d.digest());
+
+		// add thumbprint to list
+		result.add(fingerPrint);
+        android.util.Log.w("Certificate", "Certificate " + i + " thumbprint: " + fingerPrint);
+
+		// check url
+		if(!urlValid && certs[i] instanceof java.security.cert.X509Certificate)
+		{
+			String subject = ((java.security.cert.X509Certificate)certs[i]).getSubjectDN().toString();
+			android.util.Log.w("Certificate", "Certificate " + i + " subject: " + subject);
+
+			// split subject into parts
+			String [] subjectParts = subject.split(",");
+			for(int j=0; j<subjectParts.length; j++)
+			{
+				if(subjectParts[j].startsWith("CN="))
+				{
+					String certificateHost = subjectParts[j].substring(3);
+					if(certificateHost.startsWith("*."))
+					{
+						certificateHost = certificateHost.substring(2);
+					}
+
+					android.util.Log.w("Certificate", "Certificate " + i + " domain: " + certificateHost);
+
+					if(host.endsWith(certificateHost))
+					{
+						urlValid = true;
+					}
+				}
+			}
+		}
+    }
+
+	if(!urlValid)
+	{
+		return new ArrayList<String>();
+	}
+
+	return result;
   }
 
   private static String dumpHex(byte[] data) {
--- src/ios/SSLCertificateChecker.m
+++ src/ios/SSLCertificateChecker.m
@@ -3,36 +3,98 @@
 #import <Cordova/CDVPluginResult.h>
 #import <CommonCrypto/CommonDigest.h>
 
+
 @interface CustomURLConnectionDelegate : NSObject <NSURLConnectionDelegate>;
 
 @property (strong, nonatomic) CDVPlugin *_plugin;
 @property (strong, nonatomic) NSString *_callbackId;
 @property (strong, nonatomic) NSString *_allowedFingerprint;
 @property (strong, nonatomic) NSString *_allowedFingerprintAlt;
+@property (strong, nonatomic) NSString *_serverUrl;
 @property (nonatomic, assign) BOOL sentResponse;
 
-- (id)initWithPlugin:(CDVPlugin*)plugin callbackId:(NSString*)callbackId allowedFingerprint:(NSString*)allowedFingerprint allowedFingerprintAlt:(NSString*)allowedFingerprintAlt;
+- (id)initWithPlugin:(CDVPlugin*)plugin callbackId:(NSString*)callbackId allowedFingerprint:(NSString*)allowedFingerprint allowedFingerprintAlt:(NSString*)allowedFingerprintAlt url:(NSString*)url;
 
 @end
 
 @implementation CustomURLConnectionDelegate
 
-- (id)initWithPlugin:(CDVPlugin*)plugin callbackId:(NSString*)callbackId allowedFingerprint:(NSString*)allowedFingerprint allowedFingerprintAlt:(NSString*)allowedFingerprintAlt
+- (id)initWithPlugin:(CDVPlugin*)plugin callbackId:(NSString*)callbackId allowedFingerprint:(NSString*)allowedFingerprint allowedFingerprintAlt:(NSString*)allowedFingerprintAlt url:(NSString*)url
 {
 	self.sentResponse = FALSE;
 	self._plugin = plugin;
 	self._callbackId = callbackId;
 	self._allowedFingerprint = allowedFingerprint;
 	self._allowedFingerprintAlt = allowedFingerprintAlt;
+    self._serverUrl = url;
     return self;
 }
 
 // Delegate method, called from connectionWithRequest
 - (void) connection: (NSURLConnection*)connection willSendRequestForAuthenticationChallenge: (NSURLAuthenticationChallenge*)challenge {
-    NSString* fingerprint = [self getFingerprint: SecTrustGetCertificateAtIndex(challenge.protectionSpace.serverTrust, 0)];
-
+    
+    // sanity check on parameter
+    if([[NSNull null] isEqual:self._allowedFingerprint]) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"CONNECTION_NOT_SECURE"];
+        [self._plugin writeJavascript:[pluginResult toErrorCallbackString:self._callbackId]];
+    }
+    
+    BOOL fingerprintMatch = FALSE;
+    BOOL domainMatch = FALSE;
+    
+    CFIndex count = SecTrustGetCertificateCount (challenge.protectionSpace.serverTrust);
+    NSLog(@"Certificate: Certificate count: %ld", count);
+    
+    NSArray *allowedThumprints = [self._allowedFingerprint componentsSeparatedByString:@","];
+    for(int j=0; j<allowedThumprints.count; j++) {
+        NSLog(@"Certificate: allowed thumbprint %d: %@", j, allowedThumprints[j]);
+    }
+    
+    NSURL *url = [NSURL URLWithString:self._serverUrl];
+    NSString *host = [url host];
+    NSLog(@"Certificate: expected host: %@", host);
+    
+    NSString *domainPrefix = @"*.";
+    
+    for(CFIndex i=0; i<count; i++) {
+        
+        // extract thumbprint
+        SecCertificateRef cert = SecTrustGetCertificateAtIndex(challenge.protectionSpace.serverTrust, i);
+        NSString* fingerprint = [self getFingerprint: cert];
+        NSLog(@"Certificate: Certificate %ld thumbprint: %@", i, fingerprint);
+        
+        // check thumbprint if not matched yet
+        if(!fingerprintMatch) {
+            for(int j=0; j<allowedThumprints.count; j++) {
+                if([fingerprint caseInsensitiveCompare: allowedThumprints[j]] == NSOrderedSame) {
+                    fingerprintMatch = TRUE;
+                    NSLog(@"Certificate: Certificate %ld thumbprint match", i);
+                }
+            }
+        }
+        
+        CFStringRef summaryRef = SecCertificateCopySubjectSummary(cert);
+        if(summaryRef != NULL) {
+            NSString *subject = (__bridge NSString *)summaryRef;
+            NSLog(@"Certificate: Certificate %ld subject: %@", i, subject);
+            
+            if([subject hasPrefix:domainPrefix]) {
+                subject = [subject substringFromIndex:2];
+            }
+            
+            NSLog(@"Certificate: Certificate %ld normalized subject: %@", i, subject);
+            
+            if([host hasSuffix:subject]) {
+                domainMatch = TRUE;
+            }
+            
+            CFRelease(summaryRef);
+        }
+        
+    }
+    
     self.sentResponse = TRUE;
-    if ([self isFingerprintTrusted: fingerprint]) {
+    if (fingerprintMatch && domainMatch) {
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"CONNECTION_SECURE"];
         [self._plugin writeJavascript:[pluginResult toSuccessCallbackString:self._callbackId]];
     } else {
@@ -89,14 +151,14 @@
 - (void)check:(CDVInvokedUrlCommand*)command {
     NSString *serverURL = [command.arguments objectAtIndex:0];
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:serverURL]];
-                             
-                             CustomURLConnectionDelegate *delegate =
-                             [[CustomURLConnectionDelegate alloc] initWithPlugin:self callbackId:command.callbackId allowedFingerprint:[command.arguments objectAtIndex:1] allowedFingerprintAlt:[command.arguments objectAtIndex:2]];
-                             
-                             if (![NSURLConnection connectionWithRequest:request delegate:delegate]) {
-                                 CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"CONNECTION_FAILED"];
-                                 [self writeJavascript:[pluginResult toErrorCallbackString:command.callbackId]];
-                             }
-                             }
-                             
-                             @end
+    
+    CustomURLConnectionDelegate *delegate =
+    [[CustomURLConnectionDelegate alloc] initWithPlugin:self callbackId:command.callbackId allowedFingerprint:[command.arguments objectAtIndex:1] allowedFingerprintAlt:[command.arguments objectAtIndex:2] url:serverURL];
+    
+    if (![NSURLConnection connectionWithRequest:request delegate:delegate]) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"CONNECTION_FAILED"];
+        [self writeJavascript:[pluginResult toErrorCallbackString:command.callbackId]];
+    }
+}
+
+@end
